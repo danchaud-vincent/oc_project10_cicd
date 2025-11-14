8d0c238d952fcfed2a4839cbd2adea3512651525
Vincent Danchaud <vdanchaud@gmail.com>
2025-11-14 11:58:44 +0100

    create an apiUrl in environment and add it in jokes.service

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -2,25 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Joke } from '../model/joke.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JokesService {
+  private pathService = `${environment.apiUrl}/joke`;
 
-  private pathService = 'api/joke';
-
-  private subject: BehaviorSubject<Joke | null> = new BehaviorSubject<Joke | null>(null);
+  private subject: BehaviorSubject<Joke | null> =
+    new BehaviorSubject<Joke | null>(null);
 
   constructor(private httpClient: HttpClient) {
     this.getRandomJoke();
   }
 
   public getRandomJoke(): void {
-    this.httpClient.get<Joke>(this.pathService).subscribe((joke: Joke) => this.subject.next(joke));
+    this.httpClient
+      .get<Joke>(this.pathService)
+      .subscribe((joke: Joke) => this.subject.next(joke));
   }
 
-  public joke$(): Observable<Joke | null > {
+  public joke$(): Observable<Joke | null> {
     return this.subject.asObservable();
   }
 }
